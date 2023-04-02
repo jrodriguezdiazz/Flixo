1a44cf9e53a3c079ddfc05e085aa62b45d90a678
jrodriguezdiazz <jrodriguezdiazz@outlook.com>
2023-04-02 19:15:13 -0400

    ♻️ refactor: use commons component into SingUpForm.js

--- components/singUp/SingUpForm.js
+++ components/singUp/SingUpForm.js
@@ -1,50 +1,50 @@
-import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+import { Calendar } from "../commons/Calendar";
+import { TextInput } from "../commons/TextInput";
+import { TextInputPhoneNumber } from "../commons/TextInputPhoneNumber";
 
 export const SingUpForm = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder={"User name"}
-          autoCapitalize={"none"}
-          keyboardType={"email-address"}
-          textContent={"emailAddress"}
-          autoFocus={true}
-        />
+    <ScrollView>
+      <View style={styles.container}>
+        <View>
+          <TextInput
+            label="Username"
+            autofocus={true}
+          />
+          <TextInput
+            label="Password"
+            secureTextEntry={true}
+          />
+          <TextInput label="First Name" />
+          <TextInput label="Last Name" />
+          <View></View>
+          <TextInputPhoneNumber label="Pone Number" />
+          <Calendar style={styles.calendar} />
+        </View>
+        <View style={styles.disclaimer}>
+          <Text>
+            By clicking Agree & Join, you agree to the Flixo User Agreement,
+            Privacy Policy, and Cookie Policy. For phone numbers signups we will
+            send a verification code via SMS.
+          </Text>
+        </View>
+        <Pressable
+          titleSize={20}
+          style={styles.button}
+          onPress={() => navigation.push("HomeScreen")}
+        >
+          <Text style={styles.buttonText}>Agree & Join</Text>
+        </Pressable>
       </View>
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder={"Password"}
-          autoCapitalize={"none"}
-          autoCorrect={false}
-          secureTextEntry={true}
-          textContentType={"password"}
-        />
-      </View>
-      <View style={styles.forgotPasswordContainer}>
-        <Text>8 or more characters</Text>
-      </View>
-      <Pressable
-        titleSize={20}
-        style={styles.button}
-        onPress={() => navigation.push("HomeScreen")}
-      >
-        <Text style={styles.buttonText}>Continue</Text>
-      </Pressable>
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     marginHorizontal: 30,
-  },
-  inputContainer: {
-    borderRadius: 4,
-    padding: 12,
-    backgroundColor: "#fafafa",
-    marginBottom: 10,
-    borderWidth: 0.3,
+    marginBottom: 20,
   },
   button: {
     backgroundColor: "#f5d40f",
@@ -58,18 +58,11 @@ const styles = StyleSheet.create({
     color: "#ffffff",
     fontSize: 20,
   },
-  forgotPasswordContainer: {
+  disclaimer: {
     alignItems: "flex-start",
     marginVertical: 30,
-    color: "#f5d40f",
   },
-  containerSingup: {
-    flexDirection: "row",
-    width: "100%",
-    justifyContent: "center",
-    marginVertical: 30,
-  },
-  singUp: {
-    color: "#f5d40f",
+  calendar: {
+    marginTop: 10,
   },
 });
