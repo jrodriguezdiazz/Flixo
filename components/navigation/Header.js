2140fb3cfebcdbdf7d95113122d75ecdfdc95e60
jrodriguezdiazz <jrodriguezdiazz@outlook.com>
2023-04-05 20:25:41 -0400

    💄 style: fix picture profile position in Header.js

--- components/navigation/Header.js
+++ components/navigation/Header.js
@@ -5,11 +5,14 @@ import { TextInput } from "../commons/TextInput";
 export const Header = () => {
   return (
     <View style={styles.container}>
-      <ProfilePicture
-        uri={
-          "https://instagram.fsti2-1.fna.fbcdn.net/v/t51.2885-19/334683644_217978764048457_2214545457929857106_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.fsti2-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=BRPQYZWkgB0AX_c2Zpt&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfDkWrg9Jym7p4BjmdM0S8bxkV6Lq-dleuKxuaFHLD68yg&oe=641BA41A&_nc_sid=8fd12b"
-        }
-      />
+      <View style={styles.profilePicture}>
+        <ProfilePicture
+          uri={
+            "https://instagram.fsti2-1.fna.fbcdn.net/v/t51.2885-19/334683644_217978764048457_2214545457929857106_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.fsti2-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=BRPQYZWkgB0AX_c2Zpt&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfDkWrg9Jym7p4BjmdM0S8bxkV6Lq-dleuKxuaFHLD68yg&oe=641BA41A&_nc_sid=8fd12b"
+          }
+        />
+      </View>
+
       <View style={styles.searchInput}>
         <TextInput
           label={"Search"}
@@ -21,10 +24,15 @@ export const Header = () => {
 };
 const styles = StyleSheet.create({
   searchInput: {
-    width: "90%",
+    width: "80%",
   },
   container: {
     marginHorizontal: 20,
     flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  profilePicture: {
+    marginTop: 22,
+    marginHorizontal: 12,
   },
 });
