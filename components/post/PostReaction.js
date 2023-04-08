6fad8c8e02d7eb9ace48b9b6beafed6e1d1c2c7d
jrodriguezdiazz <jrodriguezdiazz@outlook.com>
2023-04-08 00:16:21 -0400

    ♻️ refactor: join fire and comment text

--- components/post/PostReaction.js
+++ components/post/PostReaction.js
@@ -1,9 +1,20 @@
 import { StyleSheet, Text, View } from "react-native";
+import { theme } from "../../utils/constant";
 
 export const PostReaction = ({ post }) => {
+  const { colors } = theme;
+  const numberOfComments = post.comments.length;
   return (
     <View style={styles.container}>
-      <Text>{post.fire.toLocaleString()} fires</Text>
+      <Text style={styles.resume(colors)}>
+        {post.fire.toLocaleString()} fires{" "}
+      </Text>
+      {numberOfComments && (
+        <Text style={styles.resume(colors)}>
+          and {numberOfComments}
+          {numberOfComments > 1 ? " comments" : " comment"}
+        </Text>
+      )}
     </View>
   );
 };
@@ -12,6 +23,9 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
     marginTop: 4,
-    marginLeft: 5,
   },
+  resume: ({ payneGrey }) => ({
+    color: payneGrey,
+    fontStyle: "italic",
+  }),
 });
