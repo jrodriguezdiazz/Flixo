909cef27d179836a8418e4195e9738e7ec2da51e
jrodriguezdiazz <jrodriguezdiazz@outlook.com>
2023-04-08 14:02:31 -0400

    ♻️ chore: add scroll view for signup screen

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,10 +1,13 @@
+import { ScrollView } from "react-native";
 import { LayoutForm } from "../components/commons/LayoutForm";
 import { SignUpForm } from "../components/sign-up/SignUpForm";
 
 export const SignUp = ({ navigation }) => {
   return (
-    <LayoutForm>
-      <SignUpForm navigation={navigation} />
-    </LayoutForm>
+    <ScrollView>
+      <LayoutForm>
+        <SignUpForm navigation={navigation} />
+      </LayoutForm>
+    </ScrollView>
   );
 };
