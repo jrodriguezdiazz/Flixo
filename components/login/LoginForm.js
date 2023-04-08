e6324b46f52e746ab6a491a89cb734128f1d69e4
jrodriguezdiazz <jrodriguezdiazz@outlook.com>
2023-04-08 17:34:37 -0400

    ✨ feature: implement login using firebase

--- components/login/LoginForm.js
+++ components/login/LoginForm.js
@@ -8,73 +8,94 @@ import {
 } from "react-native";
 import { Divider } from "react-native-elements";
 import { loginSchema } from "../../schema/login";
+import { useAuthStore } from "../../stores/useAuthStore";
 import { theme } from "../../utils/constant";
 import { Button } from "../commons/Button";
+import { Loading } from "../commons/Loading";
 import { TextInput } from "../commons/TextInput";
+import { Alert } from "./Alert";
 
 export const LoginForm = ({ navigation }) => {
+  const { error, loading, login, user } = useAuthStore((state) => ({
+    user: state.user,
+    error: state.error,
+    loading: state.loading,
+    login: state.login,
+    logout: state.logout,
+  }));
+  const handleLogin = async ({ username, password }) => {
+    await login(username, password);
+    if (user) navigation.push("HomeScreen");
+  };
+
   const { colors } = theme;
   return (
-    <Formik
-      initialValues={{
-        password: "",
-        username: "",
-      }}
-      onSubmit={(values) => console.log(values)}
-      validationSchema={loginSchema}
-      validateOnMount={true}
-    >
-      {({ handleBlur, handleChange, handleSubmit, values, isValid }) => (
-        <View style={styles.container}>
-          <TextInput
-            label={"Username"}
-            autoCapitalize={"none"}
-            keyboardType={"email-address"}
-            textContent={"emailAddress"}
-            autoFocus={true}
-            name={"username"}
-            onChangeText={handleChange("username")}
-            onBlur={handleBlur("username")}
-            value={values.username}
-          />
-          <TextInput
-            label={"Password"}
-            autoCapitalize={"none"}
-            autoCorrect={false}
-            secureTextEntry={true}
-            textContentType={"password"}
-            name={"password"}
-            onChangeText={handleChange("password")}
-            onBlur={handleBlur("password")}
-            value={values.password}
-          />
-          <View style={styles.forgotPasswordContainer(colors)}>
-            <Pressable onPress={() => navigation.push("ForgotPasswordScreen")}>
-              <Text style={styles.forgotPassword(colors)}>
-                Forgot password?
-              </Text>
-            </Pressable>
+    <>
+      <Formik
+        initialValues={{
+          password: "",
+          username: "",
+        }}
+        onSubmit={(values) => handleLogin(values)}
+        validationSchema={loginSchema}
+        validateOnMount={true}
+      >
+        {({ handleBlur, handleChange, handleSubmit, values, isValid }) => (
+          <View style={styles.container}>
+            <TextInput
+              label={"Username"}
+              autoCapitalize={"none"}
+              keyboardType={"email-address"}
+              textContent={"emailAddress"}
+              autoFocus={true}
+              name={"username"}
+              onChangeText={handleChange("username")}
+              onBlur={handleBlur("username")}
+              value={values.username}
+            />
+            <TextInput
+              label={"Password"}
+              autoCapitalize={"none"}
+              autoCorrect={false}
+              secureTextEntry={true}
+              textContentType={"password"}
+              name={"password"}
+              onChangeText={handleChange("password")}
+              onBlur={handleBlur("password")}
+              value={values.password}
+            />
+            <View style={styles.forgotPasswordContainer(colors)}>
+              <Pressable
+                onPress={() => navigation.push("ForgotPasswordScreen")}
+              >
+                <Text style={styles.forgotPassword(colors)}>
+                  Forgot password?
+                </Text>
+              </Pressable>
+            </View>
+            <Button
+              label={"Log In"}
+              icon={"login"}
+              action={handleSubmit}
+              disabled={!isValid}
+              isValid={isValid}
+            />
+            <Divider
+              width={1}
+              orientation={"vertical"}
+            />
+            <View style={styles.containerSignup}>
+              <Text>Don’t have an account? </Text>
+              <TouchableOpacity onPress={() => navigation.push("SignUpScreen")}>
+                <Text style={styles.signUp(colors)}>Sign up!</Text>
+              </TouchableOpacity>
+            </View>
           </View>
-          <Button
-            label={"Log In"}
-            icon={"login"}
-            action={handleSubmit}
-            disabled={!isValid}
-            isValid={isValid}
-          />
-          <Divider
-            width={1}
-            orientation={"vertical"}
-          />
-          <View style={styles.containerSignup}>
-            <Text>Don’t have an account? </Text>
-            <TouchableOpacity onPress={() => navigation.push("SignUpScreen")}>
-              <Text style={styles.signUp(colors)}>Sign up!</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      )}
-    </Formik>
+        )}
+      </Formik>
+      {error && Alert({ navigation })}
+      {loading && <Loading />}
+    </>
   );
 };
 
