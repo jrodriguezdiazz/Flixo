1a8ef9414a5dce4f1e011a9ac856cf80eec4ebac
jrodriguezdiazz <jrodriguezdiazz@outlook.com>
2023-04-08 00:15:40 -0400

    ♻️ refactor: use Button component in Icon.js

--- components/post/Icon.js
+++ components/post/Icon.js
@@ -1,13 +1,10 @@
-import IconReactNative from "react-native-vector-icons/Ionicons";
+import { Button } from "../commons/Button";
 
 export const Icon = ({ iconName, name }) => {
   return (
-    <IconReactNative.Button
-      name={iconName}
-      color="#000"
-      backgroundColor={"#f0f0f0ff"}
-    >
-      {name}
-    </IconReactNative.Button>
+    <Button
+      label={name}
+      icon={iconName}
+    />
   );
 };
