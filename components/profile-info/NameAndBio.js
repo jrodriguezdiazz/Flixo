aec373a1e95541a498b97df79c825172029bb338
jrodriguezdiazz <jrodriguezdiazz@outlook.com>
2023-04-09 14:57:25 -0400

    ✨ feature: implement fetchUser to get user info

--- components/profile-info/NameAndBio.js
+++ components/profile-info/NameAndBio.js
@@ -1,12 +1,29 @@
+import { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator } from "react-native-paper";
 import { USER_DATA } from "../../data/user";
+import { useAuthStore } from "../../stores/useAuthStore";
 
 export const NameAndBio = () => {
-  const { fullName = "", bio = "", username = "" } = USER_DATA;
+  const { username = "" } = USER_DATA;
+  const { user, loading, fetchUser } = useAuthStore();
+  console.log(user);
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+
+  if (!user) {
+    return <Text>No user found</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>{fullName}</Text>
-      <Text>{bio}</Text>
+      <Text>{user.displayName}</Text>
+      {user.bio && <Text>{user.bio}</Text>}
       <Text style={styles.username}>{username}</Text>
     </View>
   );
