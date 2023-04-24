5d63000d3c7ffdf446de82960d949203c8db7264
jrodriguezdiazz <jrodriguezdiazz@outlook.com>
2023-04-24 12:29:35 -0400

    ♻️ refactor: add formik

--- screens/Login.js
+++ screens/Login.js
@@ -1,5 +1,5 @@
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { useState } from "react";
+import { Formik } from "formik";
 import {
   Alert,
   SafeAreaView,
@@ -12,87 +12,102 @@ import { Button } from "../components/commons/Button";
 import { LayoutForm } from "../components/commons/LayoutForm";
 import { TextInput } from "../components/commons/TextInput";
 import { auth } from "../database/firebase";
+import { loginSchema } from "../schema/login";
 import { theme } from "../utils/constant";
 
 export const Login = ({ navigation }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const onHandleLogin = () => {
-    if (email !== "" && password !== "") {
-      signInWithEmailAndPassword(auth, email, password)
-        .then(() => console.log("Login success"))
-        .catch((err) => Alert.alert("Login error", err.message));
-    }
+  const onHandleLogin = (values) => {
+    const { email, password } = values;
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => console.log("Login success"))
+      .catch((err) => Alert.alert("Login error", err.message));
   };
 
   return (
     <LayoutForm>
       <SafeAreaView>
-        <TextInput
-          placeholder="Enter email"
-          autoCapitalize="none"
-          keyboardType="email-address"
-          textContentType="emailAddress"
-          autoFocus={true}
-          value={email}
-          onChangeText={(text) => setEmail(text)}
-        />
-        <TextInput
-          placeholder="Enter password"
-          autoCapitalize="none"
-          autoCorrect={false}
-          secureTextEntry={true}
-          textContentType="password"
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-        />
-        <Button
-          label={"Log In"}
-          icon={"login"}
-          action={onHandleLogin}
-        />
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          validationSchema={loginSchema}
+          onSubmit={onHandleLogin}
+        >
+          {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
+            <>
+              <TextInput
+                placeholder="Enter email"
+                autoCapitalize="none"
+                keyboardType="email-address"
+                textContentType="emailAddress"
+                autoFocus={true}
+                onChangeText={handleChange("email")}
+                onBlur={handleBlur("email")}
+                value={values.email}
+                errorMessage={errors.email}
+              />
+              <TextInput
+                placeholder="Enter password"
+                autoCapitalize="none"
+                autoCorrect={false}
+                secureTextEntry={true}
+                textContentType="password"
+                onChangeText={handleChange("password")}
+                onBlur={handleBlur("password")}
+                value={values.password}
+                errorMessage={errors.password}
+              />
+              <Button
+                label={"Log In"}
+                icon={"login"}
+                action={handleSubmit}
+              />
 
-        <View style={styles.container}>
-          <View>
-            <Text style={{ color: "gray", fontWeight: "600", fontSize: 14 }}>
-              {/* eslint-disable-next-line react/no-unescaped-entities */}
-              Don't have an account?
-            </Text>
-            <TouchableOpacity
-              onPress={() => navigation.navigate("SignUpScreen")}
-            >
-              <Text
-                style={{
-                  color: theme.colors.sunflower,
-                  fontWeight: "600",
-                  fontSize: 14,
-                }}
-              >
-                Sign Up
-              </Text>
-            </TouchableOpacity>
-          </View>
-          <View>
-            <Text style={{ color: "gray", fontWeight: "600", fontSize: 14 }}>
-              {/* eslint-disable-next-line react/no-unescaped-entities */}
-              Did you forget your password?{" "}
-            </Text>
-            <TouchableOpacity
-              onPress={() => navigation.navigate("ForgotPasswordScreen")}
-            >
-              <Text
-                style={{
-                  color: theme.colors.sunflower,
-                  fontWeight: "600",
-                  fontSize: 14,
-                }}
-              >
-                Reset
-              </Text>
-            </TouchableOpacity>
-          </View>
-        </View>
+              <View style={styles.container}>
+                <View>
+                  <Text
+                    style={{ color: "gray", fontWeight: "600", fontSize: 14 }}
+                  >
+                    {/* eslint-disable-next-line react/no-unescaped-entities */}
+                    Don't have an account?
+                  </Text>
+                  <TouchableOpacity
+                    onPress={() => navigation.navigate("SignUpScreen")}
+                  >
+                    <Text
+                      style={{
+                        color: theme.colors.sunflower,
+                        fontWeight: "600",
+                        fontSize: 14,
+                      }}
+                    >
+                      Sign Up
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+                <View>
+                  <Text
+                    style={{ color: "gray", fontWeight: "600", fontSize: 14 }}
+                  >
+                    {/* eslint-disable-next-line react/no-unescaped-entities */}
+                    Did you forget your password?{" "}
+                  </Text>
+                  <TouchableOpacity
+                    onPress={() => navigation.navigate("ForgotPasswordScreen")}
+                  >
+                    <Text
+                      style={{
+                        color: theme.colors.sunflower,
+                        fontWeight: "600",
+                        fontSize: 14,
+                      }}
+                    >
+                      Reset
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </>
+          )}
+        </Formik>
       </SafeAreaView>
     </LayoutForm>
   );
